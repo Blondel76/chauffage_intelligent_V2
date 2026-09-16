04056aafb42edd54f300550e29b4187e0108985e
Blondel76 <155725516+Blondel76@users.noreply.github.com>
2026-09-16 12:40:56 +0200

    Add constants for Gestion Chauffage integration

---
+++ custom_components/gestion_chauffage/const.py
@@ -0,0 +1,15 @@
+"""Constants for the Gestion Chauffage integration."""
+
+DOMAIN = "gestion_chauffage"
+
+CONF_MODE = "mode"
+CONF_HEATING_TYPE = "heating_type"
+CONF_BOILER_ENTITY = "boiler_entity"
+
+HEATING_TYPE_GAS = "gas"
+HEATING_TYPE_ELECTRIC = "electric"
+
+MODE_AUTO = "auto"
+MODE_COMFORT = "comfort"
+MODE_ECO = "eco"
+MODE_AWAY = "away"
