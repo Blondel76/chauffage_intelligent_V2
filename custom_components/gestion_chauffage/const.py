fc9250d7e62d6471ee51beb797bbc6c277b7206e
Blondel76 <155725516+Blondel76@users.noreply.github.com>
2026-09-16 16:38:31 +0200

    Update constants for Gestion Chauffage integration

--- custom_components/gestion_chauffage/const.py
+++ custom_components/gestion_chauffage/const.py
@@ -1,6 +1,14 @@
-"""Constants for the Chauffage Intelligent integration."""
+"""Constants for the Gestion Chauffage integration."""
+
+from homeassistant.const import Platform
 
 DOMAIN = "gestion_chauffage"
+
+PLATFORMS: list[Platform] = [
+    Platform.SWITCH,
+    Platform.BUTTON,
+    Platform.SENSOR,
+]
 
 CONF_MODE_SELECTOR = "mode_selector"
 CONF_HEATING_TYPE = "heating_type"
@@ -8,3 +16,15 @@ CONF_BOILER_ENTITY = "boiler_entity"
 
 HEATING_TYPE_GAS = "gas"
 HEATING_TYPE_ELECTRIC = "electric"
+
+SECURITY_STATE_GRAY = "gris"
+SECURITY_STATE_GREEN = "vert"
+SECURITY_STATE_ORANGE = "orange"
+SECURITY_STATE_RED = "rouge"
+
+SECURITY_STATES = [
+    SECURITY_STATE_GRAY,
+    SECURITY_STATE_GREEN,
+    SECURITY_STATE_ORANGE,
+    SECURITY_STATE_RED,
+]
